cc68ff107296a54701f20518918b76ed7238c5e0
seanshen20 <34725930+seanshen20@users.noreply.github.com>
2019-08-14 20:46:48 +1000

    before add hamberger bar

--- burger/src/components/Layout/Layout.js
+++ burger/src/components/Layout/Layout.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
-const layout = (props) => (
-    <>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
 
-export default layout;
+    render() {
+        return (
+            <>
+                <Toolbar />
+                <SideDrawer 
+                    open={this.state.showSideDrawer} 
+                    closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </>
+        )
+    }
+
+
+}
+
+export default Layout;
