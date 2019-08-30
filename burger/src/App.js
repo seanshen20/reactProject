924cdb5adad2969384f90bc7e4645c960040504b
seanshen20 <34725930+seanshen20@users.noreply.github.com>
2019-08-30 10:53:54 +1000

    route guard

--- burger/src/App.js
+++ burger/src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index'
+
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup()
+    console.log(this.props)
+  }
+
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to='/' />
+      </Switch>
+
+    )
+
+    if (this.props.isAuth) {
+      routes = <Switch>
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to='/' />
+      </Switch>
+    }
+
     return (
       <>
         <div>
           <Layout>
-            <Switch>
-              <Route path="/checkout" component={Checkout}></Route>
-              <Route path="/orders" component={Orders}></Route>
-              <Route path="/" exact component={BurgerBuilder}></Route>
-            </Switch>
-
+            {routes}
           </Layout>
         </div>
 
@@ -24,5 +48,15 @@ class App extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    isAuth: state.auth.token !== null
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+}
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
