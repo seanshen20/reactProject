43408d61a5ea272c45e84c7ca476d57a32bdda78
seanshen20 <34725930+seanshen20@users.noreply.github.com>
2019-09-02 12:22:15 +1000

    css animation2

--- animations--01-start/src/components/Modal/Modal.js
+++ animations--01-start/src/components/Modal/Modal.js
@@ -5,7 +5,7 @@ import './Modal.css';
 const modal = (props) => {
     const animationTiming = {
         enter: 400,
-        exit: 2000
+        exit: 1000
     }
     return (
         <CSSTransition
@@ -13,7 +13,10 @@ const modal = (props) => {
             unmountOnExit
             in={props.show}
             timeout={animationTiming}
-            classNames="fade-slide">
+            classNames={{
+                enterActive: "ModalOpen",
+                exitActive: "ModalClosed"
+            }}>
             <div className="Modal">
                 <h1>A Modal</h1>
                 <button className="Button" onClick={props.closed}>Dismiss</button>
